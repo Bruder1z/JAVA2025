8ac224a1fec934a2c96d523535ce09c138c6211a
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2025-02-12 09:45:01 -0300

    Add files via upload

---
+++ tipos-variaveis/tipos-variaveis/src/tiposEvariaveis.java
@@ -0,0 +1,17 @@
+public class tiposEvariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short NumeroCurto =1;
+        int numeroNormal = NumeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        //como definir valor imutavel
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+    }
+}
