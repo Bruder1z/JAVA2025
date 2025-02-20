c958638fb32c7f9ca1a118b63a3391d809fdf9b8
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2025-02-20 10:38:00 -0300

    Add files via upload

---
+++ Curso-POO/POO/src/main/java/com/mycompany/poo/clinical/service/ExameService.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo.clinical.service;
+
+import com.mycompany.poo.clinical.model.Exame;
+
+/**
+ *
+ * @author brudi
+ */
+public class ExameService {
+    void salvarExame(Exame exame){
+        
+        
+    }
+}
